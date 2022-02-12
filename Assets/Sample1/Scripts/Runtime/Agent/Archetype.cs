f25cc4479a48b0f44d752573e4a23ade41fe3fef
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-12 22:14:26 +0530

    Added brain to archetype.

--- Assets/Sample1/Scripts/Runtime/Agent/Archetype.cs
+++ Assets/Sample1/Scripts/Runtime/Agent/Archetype.cs
@@ -7,17 +7,19 @@ namespace AIEngineTest
         IHiraBotArchetype<NavMeshAgent>,
         IHiraBotArchetype<Animator>,
         IHiraBotArchetype<SightSensor>,
-        IHiraBotArchetype<SoundSensor>
+        IHiraBotArchetype<SoundSensor>,
+        IHiraBotArchetype<HiraLGOAPRealtimeBot>
     {
         [SerializeField] private Animator m_Animator = null;
         [SerializeField] private NavMeshAgent m_NavMeshAgent = null;
         [SerializeField] private SightSensor m_SightSensor = null;
         [SerializeField] private SoundSensor m_SoundSensor = null;
+        [SerializeField] private HiraLGOAPRealtimeBot m_Brain = null;
 
         Animator IHiraBotArchetype<Animator>.component => m_Animator;
         NavMeshAgent IHiraBotArchetype<NavMeshAgent>.component => m_NavMeshAgent;
         SightSensor IHiraBotArchetype<SightSensor>.component => m_SightSensor;
         SoundSensor IHiraBotArchetype<SoundSensor>.component => m_SoundSensor;
-        
+        HiraLGOAPRealtimeBot IHiraBotArchetype<HiraLGOAPRealtimeBot>.component => m_Brain;
     }
 }
