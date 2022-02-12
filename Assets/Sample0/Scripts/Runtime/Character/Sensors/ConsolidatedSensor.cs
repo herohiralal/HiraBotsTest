0bd0a4e5963bb08e7b1ce5ff610db2d61229ce16
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-13 02:38:22 +0530

    Created ConsolidatedSensor.

---
+++ Assets/Sample0/Scripts/Runtime/Character/Sensors/ConsolidatedSensor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Object = UnityEngine.Object;
+
+namespace AIEngineTest
+{
+    public class ConsolidatedSensor : MonoBehaviour
+    {
+        [SerializeField] private HiraBotSensor.NewObjectPerceivedEvent m_OnNewObjectPerceived;
+        [SerializeField] private HiraBotSensor.ObjectStoppedPerceivingEvent m_OnObjectStoppedPerceiving;
+
+        private readonly HashSet<Object> m_Objects = new HashSet<Object>();
+
+        public HiraBotSensor.NewObjectPerceivedEvent newObjectPerceived => m_OnNewObjectPerceived;
+        public HiraBotSensor.ObjectStoppedPerceivingEvent objectStoppedPerceiving => m_OnObjectStoppedPerceiving;
+        public ReadOnlyHashSetAccessor<Object> perceivedObjects => m_Objects.ReadOnly();
+
+        private void OnDestroy()
+        {
+            m_Objects.Clear();
+        }
+
+        public void Found(Object o)
+        {
+            if (m_Objects.Add(o))
+            {
+                m_OnNewObjectPerceived.Invoke(o);
+            }
+        }
+
+        public void Lost(Object o)
+        {
+            if (m_Objects.Remove(o))
+            {
+                m_OnObjectStoppedPerceiving.Invoke(o);
+            }
+        }
+    }
+}
--- Assets/Sample1/Scripts/Runtime/Agent/Archetype.cs
+++ Assets/Sample1/Scripts/Runtime/Agent/Archetype.cs
@@ -6,20 +6,17 @@ namespace AIEngineTest
     public class Archetype : MonoBehaviour,
         IHiraBotArchetype<NavMeshAgent>,
         IHiraBotArchetype<Animator>,
-        IHiraBotArchetype<SightSensor>,
-        IHiraBotArchetype<SoundSensor>,
+        IHiraBotArchetype<ConsolidatedSensor>,
         IHiraBotArchetype<HiraLGOAPRealtimeBot>
     {
         [SerializeField] private Animator m_Animator = null;
         [SerializeField] private NavMeshAgent m_NavMeshAgent = null;
-        [SerializeField] private SightSensor m_SightSensor = null;
-        [SerializeField] private SoundSensor m_SoundSensor = null;
+        [SerializeField] private ConsolidatedSensor m_Sensor = null;
         [SerializeField] private HiraLGOAPRealtimeBot m_Brain = null;
 
         Animator IHiraBotArchetype<Animator>.component => m_Animator;
         NavMeshAgent IHiraBotArchetype<NavMeshAgent>.component => m_NavMeshAgent;
-        SightSensor IHiraBotArchetype<SightSensor>.component => m_SightSensor;
-        SoundSensor IHiraBotArchetype<SoundSensor>.component => m_SoundSensor;
+        ConsolidatedSensor IHiraBotArchetype<ConsolidatedSensor>.component => m_Sensor;
         HiraLGOAPRealtimeBot IHiraBotArchetype<HiraLGOAPRealtimeBot>.component => m_Brain;
     }
 }
