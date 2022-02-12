40d3282114c59debf8a0e019061ef1721b51a522
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-12 19:51:14 +0530

    Added TagPropertyAttribute.

---
+++ Assets/Sample0/Scripts/Editor/GUI/PropertyDrawer/TagPropertyAttributePropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Sample0.Scripts.Editor.GUI
+{
+    [CustomPropertyDrawer(typeof(TagPropertyAttributePropertyDrawer))]
+    public class TagPropertyAttributePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.String)
+            {
+                EditorGUI.HelpBox(position, "[TagProperty] can only be used with string properties.", MessageType.Error);
+                return;
+            }
+
+            property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+        }
+    }
+}
