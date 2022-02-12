035d6628b76e40dc0fdf6f59ae0663c543fa9ba5
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-13 02:51:09 +0530

    Created StopSearchingAfterTimeServiceProvider.

---
+++ Assets/Sample1/Scripts/Runtime/Agent/Services/StopSearchingAfterTimeServiceProvider.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace AIEngineTest
+{
+    public class StopSearchingAfterTimeService : IHiraBotsService
+    {
+        public float m_WaitTime;
+        public BlackboardComponent m_Blackboard;
+        public string m_CelebrityStatusKey;
+
+        public void Start()
+        {
+        }
+
+        public void Tick(float deltaTime)
+        {
+            if (m_WaitTime < 0f)
+            {
+                return;
+            }
+
+            m_WaitTime -= deltaTime;
+
+            if (m_WaitTime < 0f)
+            {
+                m_Blackboard.SetEnumValue<CelebrityStatus>(m_CelebrityStatusKey, CelebrityStatus.Unknown);
+            }
+        }
+
+        public void Stop()
+        {
+            m_Blackboard = default;
+        }
+    }
+
+    public class StopSearchingAfterTimeServiceProvider : HiraBotsServiceProvider
+    {
+        [SerializeField] private float m_WaitTime;
+        [SerializeField] private BlackboardTemplate.KeySelector m_CelebrityStatus;
+
+        #region Validation Boilerplate
+
+        protected override void OnValidateCallback()
+        {
+            m_CelebrityStatus.keyTypesFilter = BlackboardKeyType.Enum;
+        }
+
+        protected override void Validate(System.Action<string> reportError, in BlackboardTemplate.KeySet keySet)
+        {
+            if (!m_CelebrityStatus.Validate(in keySet, BlackboardKeyType.Enum))
+            {
+                reportError("no celebrity status key");
+            }
+        }
+
+        protected override void OnTargetBlackboardTemplateChanged(BlackboardTemplate template, in BlackboardTemplate.KeySet keySet)
+        {
+            m_CelebrityStatus.OnTargetBlackboardTemplateChanged(template, in keySet);
+        }
+
+        #endregion
+
+        protected override IHiraBotsService GetService(BlackboardComponent blackboard, IHiraBotArchetype archetype)
+        {
+            return new StopSearchingAfterTimeService
+            {
+                m_Blackboard = blackboard,
+                m_CelebrityStatusKey = m_CelebrityStatus.selectedKey.name,
+                m_WaitTime = m_WaitTime
+            };
+        }
+    }
+}
