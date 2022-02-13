0bd2bced2091b563316364e6eb8f6155cc1f2556
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-13 12:25:05 +0530

    Fixed: UpdateSpeedServiceProvider.

--- Assets/Sample0/Scripts/Runtime/Character/Services/UpdateSpeedServiceProvider.cs
+++ Assets/Sample0/Scripts/Runtime/Character/Services/UpdateSpeedServiceProvider.cs
@@ -5,47 +5,34 @@ namespace AIEngineTest
 {
     public class UpdateSpeedService : IHiraBotsService
     {
-        public UpdateSpeedService(Transform transform, Animator animator, float min, float max)
+        public UpdateSpeedService(NavMeshAgent agent, Animator animator, float min, float max)
         {
-            m_Transform = transform;
+            m_NavMeshAgent = agent;
             m_Animator = animator;
             m_Min = min;
             m_Max = max;
         }
 
-        private Transform m_Transform;
+        private NavMeshAgent m_NavMeshAgent;
         private Animator m_Animator;
         private readonly float m_Min, m_Max;
 
-        private Vector3 m_Position;
-
         public void Start()
         {
-            m_Position = m_Transform.position;
         }
 
         public void Tick(float deltaTime)
         {
-            if (deltaTime < 0.005f)
-            {
-                return;
-            }
-
-            var newPos = m_Transform.position;
-            var diff = newPos - m_Position;
-            var velocity = diff / deltaTime;
-            var speed = velocity.magnitude;
+            var speed = m_NavMeshAgent.velocity.magnitude;
 
             var normalizedSpeed = (speed - m_Min) / (m_Max - m_Min);
-
-            m_Position = newPos;
 
             m_Animator.SetFloat(AnimatorHashes.s_Speed, normalizedSpeed);
         }
 
         public void Stop()
         {
-            m_Transform = null;
+            m_NavMeshAgent = null;
             m_Animator = null;
         }
     }
@@ -61,13 +48,13 @@ namespace AIEngineTest
 
         protected override IHiraBotsService GetService(BlackboardComponent blackboard, IHiraBotArchetype archetype)
         {
-            if (archetype is not IHiraBotArchetype<Animator> animatedSoldier)
+            if (archetype is not (IHiraBotArchetype<NavMeshAgent> navigatingSoldier and IHiraBotArchetype<Animator> animatedSoldier))
             {
                 Debug.LogError("Attempted to get an update speed service for an invalid game object.");
                 return null;
             }
 
-            return new UpdateSpeedService(animatedSoldier.gameObject.transform, animatedSoldier.component, m_MinSpeed, m_MaxSpeed);
+            return new UpdateSpeedService(navigatingSoldier.component, animatedSoldier.component, m_MinSpeed, m_MaxSpeed);
         }
     }
 }
