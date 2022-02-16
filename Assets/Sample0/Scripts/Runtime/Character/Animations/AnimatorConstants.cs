8f0ab49621ad2a2fc019d5b95d2383720b1f8c07
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-16 19:26:56 +0530

    Moved animation stuff to Sample0.

---
+++ Assets/Sample0/Scripts/Runtime/Character/Animations/AnimatorConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace AIEngineTest
+{
+    public static class AnimatorConstants
+    {
+        public const float k_RelaxedRunSpeed = 4.251f;
+        public const float k_UnarmedRunSpeed = 4.251f;
+        public const float k_SwordRunSpeed = 0f;
+        public const float k_SwordAndShieldRunSpeed = 0f;
+        public const float k_DualDaggersRunSpeed = 0f;
+        public const float k_StaffRunSpeed = 0f;
+    }
+}
