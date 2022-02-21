f3ec1c229329d9d3b1494704bb39c79f8384e815
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-21 07:17:16 +0530

    Created a skeleton AI for scripting/idling.

---
+++ Assets/Sample0/Scripts/Runtime/Character/Enums/ManualScriptingLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace AIEngineTest
+{
+    [System.Serializable, UnityEngine.AI.ExposedToHiraBots("9CD41184-4EF1-4C8A-9387-7BBBB31618B7")]
+    public enum ManualScriptingLevel : byte
+    {
+        None = 0,
+        Soft = 1,
+        Hard = 2
+    }
+}
