9d7ac6edad07935bc188f6bd8f6f698fb6ab1180
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-25 14:49:27 +0530

    Improved PlayMontageTaskProvider.

--- Assets/Sample0/Scripts/Runtime/Character/Tasks/PlayMontageTaskProvider.cs
+++ Assets/Sample0/Scripts/Runtime/Character/Tasks/PlayMontageTaskProvider.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,28 +6,45 @@ namespace AIEngineTest
 {
     public struct InterruptMontageMessage
     {
+        public bool m_Success;
     }
 
-    public class PlayMontageTask : IHiraBotsTask, IMessageListener<InterruptMontageMessage>
+    public struct FailMontageWithoutInterruptMessage
+    {
+    }
+
+    public class PlayMontageTask : IHiraBotsTask, IMessageListener<InterruptMontageMessage>, IMessageListener<FailMontageWithoutInterruptMessage>
     {
         private PlayMontageTask()
         {
+        }
+
+        private enum AnimationStatus
+        {
+            NotStarted,
+            Ongoing,
+            Interrupted,
+            Completed
         }
 
         private AnimatorHelper m_AnimatorHelper;
         private MontageType m_Type;
         private int? m_IntegerParam;
         private EquipmentType? m_WeaponTypeParam;
-        private HiraBotsTaskResult m_Status;
+        private AnimationStatus m_AnimationStatus;
+        private bool m_SucceedOnAnimationCompletion;
+        private float? m_Duration;
 
-        public static PlayMontageTask Get(AnimatorHelper animatorHelper, MontageType type, EquipmentType? weaponTypeParam, int? integerParam)
+        public static PlayMontageTask Get(AnimatorHelper animatorHelper, MontageType type, EquipmentType? weaponTypeParam, int? integerParam, float duration)
         {
             var output = s_Executables.Count == 0 ? new PlayMontageTask() : s_Executables.Pop();
             output.m_AnimatorHelper = animatorHelper;
             output.m_Type = type;
-            output.m_Status = HiraBotsTaskResult.InProgress;
+            output.m_AnimationStatus = AnimationStatus.NotStarted;
+            output.m_SucceedOnAnimationCompletion = true;
             output.m_WeaponTypeParam = weaponTypeParam;
             output.m_IntegerParam = integerParam;
+            output.m_Duration = duration <= 0f ? null : duration;
             return output;
         }
 
@@ -36,45 +52,46 @@ namespace AIEngineTest
 
         public void Begin()
         {
+            m_AnimatorHelper.stateEnter.AddListener(OnAnimatorStateEnter);
             m_AnimatorHelper.stateExit.AddListener(OnAnimatorStateExit);
 
             switch (m_Type)
             {
                 case MontageType.None:
-                    m_Status = HiraBotsTaskResult.Failed;
+                    m_AnimationStatus = AnimationStatus.Completed;
                     return; // no need to run the montage
                 case MontageType.MeleeAttackRight:
                     if (!m_AnimatorHelper.PrepareMeleeAttackR(m_IntegerParam))
                     {
-                        m_Status = HiraBotsTaskResult.Failed;
+                        m_AnimationStatus = AnimationStatus.Completed;
                         return;
                     }
                     break;
                 case MontageType.MeleeAttackLeft:
                     if (!m_AnimatorHelper.PrepareMeleeAttackL(m_IntegerParam))
                     {
-                        m_Status = HiraBotsTaskResult.Failed;
+                        m_AnimationStatus = AnimationStatus.Completed;
                         return;
                     }
                     break;
                 case MontageType.Unsheathe:
                     if (!m_AnimatorHelper.PrepareToEquip(m_WeaponTypeParam))
                     {
-                        m_Status = HiraBotsTaskResult.Failed;
+                        m_AnimationStatus = AnimationStatus.Completed;
                         return;
                     }
                     break;
                 case MontageType.Sheathe:
                     if (!m_AnimatorHelper.PrepareToUnequip(m_WeaponTypeParam))
                     {
-                        m_Status = HiraBotsTaskResult.Failed;
+                        m_AnimationStatus = AnimationStatus.Completed;
                         return;
                     }
                     break;
                 case MontageType.Bow:
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException();
+                    throw new System.ArgumentOutOfRangeException();
             }
 
             m_AnimatorHelper.currentMontageState = m_Type;
@@ -82,7 +99,48 @@ namespace AIEngineTest
 
         public HiraBotsTaskResult Execute(float deltaTime)
         {
-            return m_Status;
+            switch (m_AnimationStatus)
+            {
+                case AnimationStatus.NotStarted:
+                {
+                    return HiraBotsTaskResult.InProgress;
+                }
+                case AnimationStatus.Ongoing:
+                {
+                    UpdateDuration(deltaTime);
+                    m_Duration -= deltaTime;
+                    return HiraBotsTaskResult.InProgress;
+                }
+                case AnimationStatus.Interrupted:
+                {
+                    return HiraBotsTaskResult.Failed;
+                }
+                case AnimationStatus.Completed:
+                {
+                    UpdateDuration(deltaTime);
+                    if (m_Duration.HasValue)
+                    {
+                        return HiraBotsTaskResult.InProgress;
+                    }
+
+                    return m_SucceedOnAnimationCompletion ? HiraBotsTaskResult.Succeeded : HiraBotsTaskResult.Failed;
+                }
+                default:
+                {
+                    throw new System.ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private void UpdateDuration(float deltaTime)
+        {
+            if (!m_Duration.HasValue) return;
+
+            m_Duration = m_Duration.Value - deltaTime;
+            if (m_Duration.Value > 0f) return;
+
+            m_AnimatorHelper.keepMontageActive = false;
+            m_Duration = null;
         }
 
         public void Abort()
@@ -98,30 +156,47 @@ namespace AIEngineTest
 
         private void Recycle()
         {
+            m_AnimatorHelper.keepMontageActive = false;
+            m_AnimatorHelper.stateEnter.RemoveListener(OnAnimatorStateEnter);
             m_AnimatorHelper.stateExit.RemoveListener(OnAnimatorStateExit);
             m_AnimatorHelper = null;
-            m_Type = MontageType.None;
-            m_Status = HiraBotsTaskResult.InProgress;
-            m_WeaponTypeParam = null;
-            m_IntegerParam = null;
             s_Executables.Push(this);
+        }
+
+        private void OnAnimatorStateEnter(MontageType type)
+        {
+            if (m_Type == type && m_AnimationStatus == AnimationStatus.NotStarted)
+            {
+                m_AnimationStatus = AnimationStatus.Ongoing;
+
+                if (m_Duration.HasValue)
+                {
+                    m_AnimatorHelper.keepMontageActive = true;
+                }
+            }
         }
 
         private void OnAnimatorStateExit(MontageType type)
         {
-            if (m_Type == type && m_Status == HiraBotsTaskResult.InProgress)
+            if (m_Type == type && m_AnimationStatus == AnimationStatus.Ongoing)
             {
-                m_Status = HiraBotsTaskResult.Succeeded;
+                m_AnimationStatus = AnimationStatus.Completed;
             }
         }
 
         public void OnMessageReceived(InterruptMontageMessage message)
         {
-            if (m_Status == HiraBotsTaskResult.InProgress)
+            if (m_AnimationStatus != AnimationStatus.Interrupted)
             {
+                m_AnimationStatus = AnimationStatus.Interrupted;
+                m_SucceedOnAnimationCompletion = message.m_Success && m_SucceedOnAnimationCompletion;
                 m_AnimatorHelper.currentMontageState = MontageType.None;
-                m_Status = HiraBotsTaskResult.Failed;
             }
+        }
+
+        public void OnMessageReceived(FailMontageWithoutInterruptMessage message)
+        {
+            m_SucceedOnAnimationCompletion = false;
         }
     }
 
@@ -131,6 +206,7 @@ namespace AIEngineTest
         [SerializeField] private bool m_UseCustomParam;
         [SerializeField] private int m_IntegerParam;
         [SerializeField] private EquipmentType m_EquipmentTypeParam;
+        [SerializeField] private float m_Duration;
 
         protected override IHiraBotsTask GetTask(BlackboardComponent blackboard, IHiraBotArchetype archetype)
         {
@@ -140,7 +216,8 @@ namespace AIEngineTest
                     animated.component,
                     m_Type,
                     m_UseCustomParam ? m_EquipmentTypeParam : null,
-                    m_UseCustomParam ? m_IntegerParam : null);
+                    m_UseCustomParam ? m_IntegerParam : null,
+                    m_Duration);
             }
 
             return null;
