6b6a585327d86d337c26de69b368f894444f7b4b
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-03-15 05:12:15 +0530

    Removed fighter stuff and added golden rules to AbilitiesAndVariables.

--- Assets/Sample2/Scripts/Runtime/AI/AbilitiesAndVariables.cs
+++ Assets/Sample2/Scripts/Runtime/AI/AbilitiesAndVariables.cs
@@ -4,58 +4,27 @@ using UnityEngine;
 
 namespace AIEngineTest
 {
+    /**
+     * Golden rules:
+     * 1. Every action must be usable for more than one goal.
+     * 2. Every goal-target must have more than one action that's able to finish it.
+     * 3. Every plan must be at least 2 actions long.
+     */
     internal static class AbilitiesAndVariables
     {
         private struct Fighter
         {
-            private float Stamina;
-            private float BoredomFromCurrentEnemy;
-            private float Aggression; // increases w every attack, reduce by 50 every kill
+            
+        }
 
-            private void CatchBreath()
-            {
-                Stamina += 100f;
-            }
+        private struct Rogue
+        {
+            
+        }
 
-            private void SingleAttack()
-            {
-                if (Stamina >= 15)
-                {
-                    Stamina -= Random.Range(10f, 15f);
-                    BoredomFromCurrentEnemy += Random.Range(10f, 20f);
-                    Aggression += Random.Range(5f, 10f);
-                }
-            }
-
-            private void ComboAttack2Hit()
-            {
-                if (Aggression >= 30 && Stamina >= 25)
-                {
-                    Stamina -= Random.Range(20f, 25f);
-                    BoredomFromCurrentEnemy += Random.Range(15f, 20f);
-                    Aggression += Random.Range(10f, 20f);
-                }
-            }
-
-            private void ComboAttack3Hit()
-            {
-                if (Aggression >= 50 && Stamina >= 37.5)
-                {
-                    Stamina -= Random.Range(30f, 37.5f);
-                    BoredomFromCurrentEnemy += Random.Range(17.5f, 20f);
-                    Aggression += Random.Range(15f, 30f);
-                }
-            }
-
-            private void ChargeToDifferentEnemy()
-            {
-                if (BoredomFromCurrentEnemy >= 100f && Stamina >= 45f)
-                {
-                    Stamina -= Random.Range(35f, 45f);
-                    BoredomFromCurrentEnemy = 0f;
-                    Aggression += Random.Range(0f, 5f);
-                }
-            }
+        private struct Wizard
+        {
+            
         }
     }
 }
