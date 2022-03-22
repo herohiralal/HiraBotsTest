d9883c55a44881dd2dee97577017239055ba6fa1
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-03-22 21:04:10 +0530

    Fixed: CurrentEnemyTrackerServiceProvider.

--- Assets/Sample0/Scripts/Runtime/Character/Services/CurrentEnemyTrackerServiceProvider.cs
+++ Assets/Sample0/Scripts/Runtime/Character/Services/CurrentEnemyTrackerServiceProvider.cs
@@ -21,9 +21,6 @@ namespace AIEngineTest
         private IHiraBotArchetype m_Self;
         private BlackboardComponent m_Blackboard;
 
-        private float m_MeleeDistance;
-        private float m_ShortDistance;
-
         private static readonly Stack<CurrentEnemyTrackerService> s_Executables = new Stack<CurrentEnemyTrackerService>();
 
         public void Start()
@@ -51,34 +48,22 @@ namespace AIEngineTest
 
                 {
                     var dist = Vector3.Distance(selfPos, otherPos);
-                    if (dist <= m_MeleeDistance)
+                    trd = dist switch
                     {
-                        trd = TargetRelativeDistance.Melee;
-                    }
-                    else if (dist <= m_ShortDistance)
-                    {
-                        trd = TargetRelativeDistance.Short;
-                    }
-                    else
-                    {
-                        trd = TargetRelativeDistance.Long;
-                    }
+                        <= 2.0f => TargetRelativeDistance.Melee,
+                        <= 7.5f => TargetRelativeDistance.Short,
+                        _ => TargetRelativeDistance.Long
+                    };
                 }
 
                 {
                     var dot = Vector3.Dot(otherTransform.forward, (selfPos - otherPos).normalized);
-                    if (dot >= 0.5f)
+                    trp = dot switch
                     {
-                        trp = TargetRelativePosition.InFront;
-                    }
-                    else if (dot >= -0.5f)
-                    {
-                        trp = TargetRelativePosition.Flanking;
-                    }
-                    else
-                    {
-                        trp = TargetRelativePosition.Sneaking;
-                    }
+                        >= 0.5f => TargetRelativePosition.InFront,
+                        >= -0.5f => TargetRelativePosition.Flanking,
+                        _ => TargetRelativePosition.Sneaking
+                    };
                 }
             }
 
@@ -88,6 +73,8 @@ namespace AIEngineTest
 
         public void Stop()
         {
+            Tick(0f);
+
             m_Blackboard = default;
             s_Executables.Push(this);
         }
@@ -95,6 +82,11 @@ namespace AIEngineTest
 
     public class CurrentEnemyTrackerServiceProvider : HiraBotsServiceProvider
     {
+        public CurrentEnemyTrackerServiceProvider()
+        {
+            tickInterval = 0.1f;
+        }
+
         protected override IHiraBotsService GetService(BlackboardComponent blackboard, IHiraBotArchetype archetype)
         {
             return CurrentEnemyTrackerService.Get(archetype, blackboard);
